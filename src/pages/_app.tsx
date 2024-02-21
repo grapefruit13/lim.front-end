ea711d45a4175973ee677e678dd180e1285137ad
grapefruit13 <liam.heo11@gmail.com>
2024-02-21 23:27:00 +0900

    style: Apply google fonts and SCSS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import "@/styles/base/common.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
