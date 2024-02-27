3bf57251d410902b7fa3d5519096c5d0ed1b61af
grapefruit13 <liam.heo11@gmail.com>
2024-02-28 00:29:28 +0900

    feat: implement main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,43 @@
-import Head from "next/head";
+import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import Profile from '@/components/common/Profile';
+import { allPosts } from '@/contentlayer/generated';
+import PostCard from '@/components/common/PostCard';
 
-export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>lim.dev</title>
-        <meta name="description" content="Welcome to lim dev blog" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/svg/favicon.svg" />
-      </Head>
-      <div>hello world this is main page</div>
-    </>
-  );
+interface PostProps {
+  title: string;
+  description: string;
+  categories: string[];
+  thumbnail?: string;
+  createdAt: string;
+  minsToRead: number;
 }
+
+interface MainPageProps {
+  posts: PostProps[];
+}
+
+const MainPage = ({ posts }: MainPageProps) => (
+  <>
+    <Head>
+      <title>lim.Front-end</title>
+      <meta name='description' content='Welcome to lim dev blog' />
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <link rel='icon' href='/svg/favicon.svg' />
+    </Head>
+    <Profile />
+    <PostCard posts={posts} />
+  </>
+);
+
+export default MainPage;
+
+export const getStaticProps: GetStaticProps = () => {
+  const posts: PostProps[] = allPosts;
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
