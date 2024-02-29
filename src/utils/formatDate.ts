a0e910709e9a7801c4d621e606109e534fcab751
grapefruit13 <liam.heo11@gmail.com>
2024-03-01 02:17:22 +0900

    refactor: use date-fns to format date

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,39 +1,8 @@
-const convertToEnglishMonth = (monthNumber: string) => {
-  switch (monthNumber) {
-    case '01':
-      return 'Jan';
-    case '02':
-      return 'Feb';
-    case '03':
-      return 'Mar';
-    case '04':
-      return 'Apr';
-    case '05':
-      return 'May';
-    case '06':
-      return 'Jun';
-    case '07':
-      return 'Jul';
-    case '08':
-      return 'Aug';
-    case '09':
-      return 'Sep';
-    case '10':
-      return 'Oct';
-    case '11':
-      return 'Nov';
-    case '12':
-      return 'Dec';
-    default:
-      return 'Invalid month';
-  }
-};
+import { format } from 'date-fns';
 
-const formatDate = (date: string) => {
-  const fullDate = date.split('T');
-  const dateArr = fullDate[0].split('-');
-
-  return `${convertToEnglishMonth(dateArr[1])} ${dateArr[2]}, ${dateArr[0]} `;
+const formatDate = (date: Date) => {
+  const formattedDate = format(date, 'MMM dd, yyyy');
+  return formattedDate;
 };
 
 export default formatDate;
